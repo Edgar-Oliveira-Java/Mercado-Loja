e5842a64f65cf892e8e60eaa2a8c7766bdb38e47
Edgar Oliveira Souza <edgaroliveirasouza1@gmail.com>
2026-06-01 15:56:29 -0300

    Adicionado muitos mais produtos ao estoque

--- src/krill/dominio/Estoque.java
+++ src/krill/dominio/Estoque.java
@@ -6,6 +6,13 @@ public class Estoque {
         estoque[0] = new Produto("Bife", 19.90, "Carnes");
         estoque[1] = new Produto("Alface", 5, "Legumes e Verduras");
         estoque[2] = new Produto("Detergente", 3.40, "Limpeza");
+        estoque[3] = new Produto("Hamburguer", 24.40, "Carnes");
+        estoque[4] = new Produto("Papricá", 1.50, "Temperos");
+        estoque[5] = new Produto("Cenoura", 8.50, "Legumes e verduras");
+        estoque[6] = new Produto("Bolo", 12.30, "Doces e Padaria");
+        estoque[7] = new Produto("Pão frances", 6, "Doces e Padaria");
+        estoque[8] = new Produto("Beterraba", 12.50, "Legumes e Verduras");
+        estoque[9] = new Produto("Café", 34.90, "Alimentos");
     }
 
     public void VerificarEstoque(){
